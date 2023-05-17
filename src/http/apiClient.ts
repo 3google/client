ecec581a4557664911460908deb1a6e53306ca55
mieum12 <qpdlqltb1215@gmail.com>
2023-05-17 10:17:28 +0900

    fix 서버 url /api 붙이기 수정

--- src/http/apiClient.ts
+++ src/http/apiClient.ts
@@ -2,6 +2,6 @@ import { SERVER_URL } from '../common/constants';
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: `${SERVER_URL}`,
+  baseURL: `${SERVER_URL}/api`,
   withCredentials: true, // 쿠키사용
 });
